950456a5da626fbe820a35e8053d745039c67e26
Will Washburn <will.washburn@gmail.com>
2013-06-02 00:53:11 -0400

    only add on change

--- checkBikeLevels.php
+++ checkBikeLevels.php
@@ -1,38 +1,40 @@
 <?php
 
-$stationsUrl = 'http://citibikenyc.com/stations/json';
-$host = '127.0.0.1';
-$dbname = 'citi_bike';
-$user= 'citibike';
-$passwd = 'citibikewoo';
+    $stationsUrl = 'http://citibikenyc.com/stations/json';
+    $host        = '127.0.0.1';
+    $dbname      = 'citi_bike';
+    $user        = 'citibike';
+    $passwd      = 'citibikewoo';
 
-/*
- * Grab that shiznip
- */
-
-
-try {
-	$DBO = new PDO(
-	    'mysql:host=' . $host . ';dbname=' . $dbname . ';port=3306',
-	    $user,
-	    $passwd
-	);
-
-	$ch= curl_init();
-	curl_setopt($ch, CURLOPT_URL, $stationsUrl);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$stationData = json_decode(curl_exec($ch), true);
-	curl_close($ch);
+    /*
+     * Grab that shiznip
+     */
 
 
-	foreach ($stationData['stationBeanList'] as $station) {
+    try {
 
-		$sql = 'select id from stations where id = :id';
-		$STH = $DBO->prepare($sql);
-		$result = $STH->execute(array(':id' => $station['id']));
-		
-		if ($STH->rowCount() == 0) {
-			$sql = "insert into stations set id = :id,
+        $DBO = new PDO(
+            'mysql:host=' . $host . ';dbname=' . $dbname . ';port=3306',
+            $user,
+            $passwd
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $stationsUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $stationData = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+
+        foreach ($stationData['stationBeanList'] as $station) {
+
+            $sql    = 'select id from stations where id = :id';
+            $STH    = $DBO->prepare($sql);
+            $result = $STH->execute(array(':id' => $station['id']));
+
+            if ($STH->rowCount() == 0) {
+
+                $sql = "insert into stations set
+                            id = :id,
 							stationName = :stationName,
 							latitude = :latitude,
 							longitude = :longitude,
@@ -43,31 +45,54 @@ try {
 							postalCode = :postalCode,
 							location = :location,
 							altitude = :altitude";
-			$STH = $DBO->prepare($sql);
-			$STH->bindParam(':id',$station['id']);
-			$STH->bindParam(':stationName', $station['stationName']);
-			$STH->bindParam(':latitude' , $station['latitude']);
-			$STH->bindParam(':longitude',$station['longitude']);
-			$STH->bindParam(':streetAddress1', $station['stAddress1']);
-			$STH->bindParam(':streetAddress2', $station['stAddress2']);
-			$STH->bindParam(':landmark',$station['landMark']);
-			$STH->bindParam(':city', $station['city']);
-			$STH->bindParam(':postalCode', $station['postalCode']);
-			$STH->bindParam(':location', $station['location']);
-			$STH->bindParam(':altitude', $station['altitude']);
-			
-			$STH->execute();
-				
-		}
 
-		//$sql = 'select totalDocks,availableDocks,status,statusCode from station_status where station_id =:id order by timestamp asc limit 1';
-		//$STH = $DBH->
+                $STH = $DBO->prepare($sql);
 
-		
-		$time = time();
-		$availableBikes = $station['totalDocks'] - $station['availableDocks'];
+                $STH->bindParam(':id', $station['id']);
+                $STH->bindParam(':stationName', $station['stationName']);
+                $STH->bindParam(':latitude', $station['latitude']);
+                $STH->bindParam(':longitude', $station['longitude']);
+                $STH->bindParam(':streetAddress1', $station['stAddress1']);
+                $STH->bindParam(':streetAddress2', $station['stAddress2']);
+                $STH->bindParam(':landmark', $station['landMark']);
+                $STH->bindParam(':city', $station['city']);
+                $STH->bindParam(':postalCode', $station['postalCode']);
+                $STH->bindParam(':location', $station['location']);
+                $STH->bindParam(':altitude', $station['altitude']);
 
-		$sql = "insert into station_status set station_id = :id,
+                $STH->execute();
+
+            }
+
+            $sql = 'select totalDocks,availableDocks,status,statusCode from station_status where station_id =:id order by timestamp asc limit 1';
+            $STH = $DBO->prepare($sql);
+            $STH->execute(array(':id' => $station['id']));
+
+            if ($STH->rowCount() > 0) {
+
+                $result                 = $STH->fetch();
+                $previousDocks          = $result['totalDocks'];
+                $previousAvailableDocks = $result['availableDocks'];
+                $previousStatus         = $result['status'];
+                $previousStatusCode     = $result['statusCode'];
+
+            } else {
+                $previousAvailableDocks = false;
+                $previousDocks          = false;
+                $previousStatus         = false;
+                $previousStatusCode     = false;
+            }
+
+
+            if ($previousAvailableDocks != $station['availableDocks']
+                OR  $previousDocks != $station['totalDocks']
+                OR $previousStatusCode != $station['statusKey']
+                OR $previousStatus != $station['statusValue']
+            ) {
+
+                $availableBikes = $station['totalDocks'] - $station['availableDocks'];
+
+                $sql = "insert into station_status set station_id = :id,
 							totalDocks = :totalDocks,
 							availableDocks = :availableDocks, 
 							availableBikes = :availableBikes,
@@ -76,27 +101,28 @@ try {
 							lastCommunicationTime = :lastCommunicationTime,
 							testStation = :testStation,
 							timestamp = :timestamp";
-		$STH = $DBO->prepare($sql);
+                $STH = $DBO->prepare($sql);
 
-		$STH->bindParam(':id',$station['id']);
-		$STH->bindParam(':totalDocks' ,$station['totalDocks']);
-		$STH->bindParam(':availableDocks' ,$station['availableDocks']);
-		$STH->bindParam(':availableBikes', $availableBikes);
-		$STH->bindParam(':status',$station['statusValue']);
-		$STH->bindParam(':statusCode' ,$station['statusKey']);
-		$STH->bindParam(':lastCommunicationTime' ,$station['lastCommunicationTime']);
-		$STH->bindParam(':testStation' ,$station['testStation']);
-		$STH->bindParam(':timestamp' ,$time);
+                $STH->bindParam(':id', $station['id']);
+                $STH->bindParam(':totalDocks', $station['totalDocks']);
+                $STH->bindParam(':availableDocks', $station['availableDocks']);
+                $STH->bindParam(':availableBikes', $availableBikes);
+                $STH->bindParam(':status', $station['statusValue']);
+                $STH->bindParam(':statusCode', $station['statusKey']);
+                $STH->bindParam(':lastCommunicationTime', $station['lastCommunicationTime']);
+                $STH->bindParam(':testStation', $station['testStation']);
+                $STH->bindParam(':timestamp', $time);
 
-		$STH->execute();
+                $STH->execute();
 
-	echo $station['stationName'].' updated.'. PHP_EOL;
+                echo $station['stationName'] . ' updated.' . PHP_EOL;
+            } else {
+                echo $station['stationName']. ' has nothing to update'. PHP_EOL;
+            }
 
-	}
-}
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 
-catch (PDOException $e) {
-	echo $e->getMessage();
-}	
-
-echo 'Script completed'. PHP_EOL;
+    echo 'Script completed' . PHP_EOL;
