b462a99af8764fdbf42b6dedaf52e4aa834155a6
yeshwanthyk <yeshwanth.yk@gmail.com>
2013-06-02 02:43:50 -0400

    Remove commented lines and add docs

--- ratings.py
+++ ratings.py
@@ -1,15 +1,17 @@
 from datetime import datetime, date, timedelta
 
+#MUST install mysql-python
+#To do that just run "pip install mysql-python"
 import MySQLdb
 
+#DB Settings
 DB_HOST = "localhost"
-DB_USER = "neo"
-DB_PASSWD = "mysql"
-DB_NAME = "citi1"
+DB_USER = ""
+DB_PASSWD = ""
+DB_NAME = ""
 
 stations = []
 times = []
-pp = []
 
 print "------Created empty Lists------"
 
@@ -21,11 +23,13 @@ for y in range(30,1470,30):
 
 print "------Created date ranges------"
 
+#Connection to Db
 mysql_connection = MySQLdb.connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME)
 mysql_cur = mysql_connection.cursor()
 
 print "------Created Cursor------"
 
+#Getting a list of stations
 sql = """select `id` from stations"""
 mysql_cur.execute(sql)
 stations_out = mysql_cur.fetchall()
@@ -34,6 +38,8 @@ for x in stations_out:
 
 print "------Created Stations------"
 
+#Creating Ratings Table
+#TODO:Discuss addition of fields
 sql_table_ratings = """\
 CREATE TABLE IF NOT EXISTS ratings (
   id int(11) unsigned NOT NULL auto_increment,
@@ -47,23 +53,21 @@ CREATE TABLE IF NOT EXISTS ratings (
 mysql_cur.execute(sql_table_ratings)
 print "------Created Ratings Table------"
 
+#Aggregating ratings
+#Currently averages for availableBikes and availableDocks
+#TODO: Fix PEP-8 after Demo
 for i in stations:
     for j in range(0, len(times)-1):
         sql1 = """select %s, avg(`availableBikes`), avg(`availableDocks`),
         %s from station_status where `timestamp` > %s and `timestamp` <  %s and
         `station_id` = %s""" % (i,times[j+1],times[j],times[j+1],i)
-        print i,j
+
+        print "Calculating for Station:" + str(i)
+
         mysql_cur.execute(sql1)
         ans = mysql_cur.fetchone()
         if ans[1]:
-            #pp.append(ans)
             sql2 = """insert into ratings(id, station_id, avgAvailableBikes,
             avgAvailableDocks, timestamp)values (NULL, %s, %s, %s, %s)""" % (ans[0], ans[1],
             ans[2], ans[3])
             mysql_cur.execute(sql2)
-
-#for i in pp:
-    #sql = """insert into ratings(id, station_id, avgAvailableBikes,
-    #avgAvailableDocks, timestamp)values (NULL, %s, %s, %s, %s)""" % (i[0], i[1],
-    #i[2], i[3])
-    #mysql_cur.execute(sql)
